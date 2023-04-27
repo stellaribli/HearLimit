8f859aa7e432fd4d61cd8282227bfb7a95ee1919
Stella Ribli <18219027@std.stei.itb.ac.id>
2023-04-27 19:37:27 +0700

    update

--- app/src/main/java/com/example/hearlimit/MainActivity.java
+++ app/src/main/java/com/example/hearlimit/MainActivity.java
@@ -7,10 +7,12 @@ import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private Button checkHeadphoneButton;
     private AudioManager audioManager;
     private HeadphoneReceiver headphoneReceiver;
-
+    private TextView decibelTextView;
     Button lanjutbutton;
     int score = 0;
 
     private class HeadphoneReceiver extends BroadcastReceiver {
+        private long headphonesPluggedInTime;
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -31,10 +34,14 @@ public class MainActivity extends AppCompatActivity {
                 if (state == 1) {
                     // Headphones are plugged in
                     headphoneStatusTextView.setText("Headphone Status: Plugged In");
+                    headphonesPluggedInTime = System.currentTimeMillis();
 //                    showNotification("Headphone Status: Plugged In");
                 } else {
                     // Headphones are unplugged
-                    headphoneStatusTextView.setText("Headphone Status: Not Plugged In");
+                    long duration = System.currentTimeMillis() - headphonesPluggedInTime; // Calculate the duration
+                    headphoneStatusTextView.setText("Headphone Status: Not Plugged In\nHeadphone Plugged In Duration: " + duration/1000 + " seconds");
+
+
                 }
             }
         }
@@ -67,7 +74,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        // Get the UI element
+        decibelTextView = (TextView) findViewById(R.id.decibel_textview);
+
+        // Get the AudioManager service
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        // Set up a periodic task to update the decibel level every second
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                updateDecibelLevel();
+            }
+        }, 0, 1, TimeUnit.SECONDS);
     }
+    private void updateDecibelLevel() {
+        // Get the current volume level for the music stream
+        int volumeLevel = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        // Convert the volume level to decibels
+        float decibels = 20 * (float) Math.log10(volumeLevel);
+
+        // Update the UI to display the current decibel level
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                decibelTextView.setText("Current Decibel Level: " + decibels + " dB");
+            }
+        });
+    }
+
     private boolean isHeadphonesPlugged(){
         AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         AudioDeviceInfo[] audioDevices = audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS);
